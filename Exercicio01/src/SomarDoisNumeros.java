78713210dd8d7b481ef720eae485a67da55b3709
Toni Lommez <tonilommez@hotmail.com>
2022-08-23 17:45:23 -0300

    First Commit

---
+++ Exercicio01/src/SomarDoisNumeros.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class SomarDoisNumeros {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String args[]) {
+		// Declaracao de variaveis
+		double num1, num2, soma;
+		
+		// Leituras
+		System.out.println("Digite um numero:");
+		num1 = sc.nextDouble();
+		System.out.println("Digite outro numero");
+		num2 = sc.nextDouble();
+		
+		// Somar
+		soma = num1 + num2;
+		
+		// Mostrar na tela
+		System.out.println("Soma: " + soma);
+	}
+}
